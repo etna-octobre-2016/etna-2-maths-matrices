8d32d16ce120bb4eb6741ac1ab5d11988ae5d1bb
Said AHEMT <said.ahemt@nolaroads.com>
2014-12-21 16:56:18 +0100

    recharge la page lors de la modification d'un composant polymer

--- public/gulp/tasks/init.js
+++ public/gulp/tasks/init.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 
 gulp.task('default', ['clean', 'browser-sync', 'sass'], function(){
     gulp.watch('src/sass/*.scss', ['sass']);
-    gulp.watch('*.html', ['browser-sync-reload']);
+    gulp.watch(['*.html', 'src/html/**/*.html'], ['browser-sync-reload']);
 });
