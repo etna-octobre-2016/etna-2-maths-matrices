c6057a0ce9e1584a644118665039c0ead671d01d
Said AHEMT <said.ahemt@nolaroads.com>
2014-12-21 16:40:37 +0100

    ajoute le rafraichissement des pages avec browser-sync lors de la modification du HTML

--- public/gulp/tasks/init.js
+++ public/gulp/tasks/init.js
@@ -2,4 +2,5 @@ var gulp = require('gulp');
 
 gulp.task('default', ['clean', 'browser-sync', 'sass'], function(){
     gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('*.html', ['browser-sync-reload']);
 });
