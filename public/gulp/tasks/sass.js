07a34f11b52e0788d27a41174675d2764946b2c7
Said AHEMT <said.ahemt@nolaroads.com>
2014-12-21 21:50:37 +0100

    des trucs

--- public/gulp/tasks/sass.js
+++ public/gulp/tasks/sass.js
@@ -2,11 +2,20 @@ var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     sassConfig  = { errLogToConsole: true };
 
-gulp.task('sass', ['sass_components']);
+gulp.task('sass', ['sass_main', 'sass_components']);
 
 gulp.task('sass_components', function(){
 
-    return gulp.src(['./src/components/**/*.scss'], {base: '.'})
+    return gulp
+        .src(['./src/components/**/*.scss'], {base: '.'})
         .pipe(sass(sassConfig))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('sass_main', function(){
+
+    return gulp
+        .src(['src/sass/*.scss'])
+        .pipe(sass(sassConfig))
+        .pipe(gulp.dest('dist/css'));
 });
