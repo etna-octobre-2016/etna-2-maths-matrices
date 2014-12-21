08402954210cb03082879c521eac2fa6442b76bd
Said AHEMT <said.ahemt@nolaroads.com>
2014-12-21 18:17:17 +0100

    finalise tout ce qui touche au reload

--- public/gulp/tasks/init.js
+++ public/gulp/tasks/init.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('default', ['clean', 'browser-sync', 'sass'], function(){
-    gulp.watch('src/sass/*.scss', ['sass']);
-    gulp.watch(['*.html', 'src/html/**/*.html'], ['browser-sync-reload']);
+    gulp.watch('src/**/*.scss', ['sass', 'browser-sync-reload']);
+    gulp.watch(['*.html', 'src/**/*.html'], ['browser-sync-reload']);
 });
--- public/gulp/tasks/sass.js
+++ public/gulp/tasks/sass.js
@@ -2,10 +2,11 @@ var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     sassConfig  = { errLogToConsole: true };
 
-gulp.task('sass', function(){
+gulp.task('sass', ['sass_components']);
 
-    return gulp.src('src/sass/*.scss')
+gulp.task('sass_components', function(){
+
+    return gulp.src(['./src/components/**/*.scss'], {base: '.'})
         .pipe(sass(sassConfig))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(global.browserSyncReload({ stream:true }));
+        .pipe(gulp.dest('.'));
 });
