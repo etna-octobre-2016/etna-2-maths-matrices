c02297126376057f57d79a5a762f05be6cd9d9ea
Said AHEMT <said.ahemt@nolaroads.com>
2015-01-17 14:29:54 +0100

    quelques tests avec CanJS

--- public/src/js/src/index.js
+++ public/src/js/src/index.js
@@ -3,16 +3,17 @@ var $   = require("../../vendors/zepto/zepto"),
     Can = require("../../vendors/canjs/can.zepto");
 
 // Templates
-// var todos = new Can.List([
-//     {name: "Faire les courses"},
-//     {name: "Apprendre CanJS"},
-//     {name: "Aller aux toilettes"}
-// ]);
+var todos = new Can.List([
+    {name: "Faire les courses"},
+    {name: "Apprendre CanJS"},
+    {name: "Aller aux toilettes"}
+]);
 
-console.log($);
-console.log(Can);
+console.log(todos);
 
-console.log($("body"));
+$("body").html(Can.view("templates/test.ejs", {todos: todos}));
 
-// $("body").html(Can.view("templates/test.ejs", todos));
+window.test = function(){
 
+    todos.attr(1, {name:"Maitriser CanJS"});
+};
