c932f9f6ee15954b06a53116a7ed5eb9f2908d0c
Saïd AHEMT <said.ahemt@nolaroads.com>
2015-11-30 21:40:13 +0100

    builds sources

--- dist/components/polynomial-roots/polynomial-roots.js
+++ dist/components/polynomial-roots/polynomial-roots.js
@@ -10,7 +10,15 @@ define(function(require){
         data: function() {
 
             return {
+                a0: "",
+                a1: "",
+                a2: "",
+                a3: "",
                 currentState: "default",
+                isA0Invalid: false,
+                isA1Invalid: false,
+                isA2Invalid: false,
+                isA3Invalid: false,
                 isMenuOpened: false
             };
         },
@@ -27,9 +35,62 @@ define(function(require){
             this.typeWrite(this.$$.welcomeTitle, message, 3000);
         },
         methods: {
+            getCoefficientsArray: function()
+            {
+                return [
+                    this.a0,
+                    this.a1,
+                    this.a2,
+                    this.a3
+                ];
+            },
+            isCoefficientValid: function(coefficient) {
+                
+                var value = new Number(coefficient);
+                
+                if (!isNaN(value))
+                {
+                    return true;
+                }
+                return false;
+            },
+            observeCoefficients: function() {
+                
+                var coefficients,
+                    i,
+                    isCoefficientsListComplete,
+                    length;
+                
+                console.log("observeCoefficients");
+                
+                coefficients = this.getCoefficientsArray();
+                length = coefficients.length;
+                isCoefficientsListComplete = true;
+                for (i = 0; i < length; i++)
+                {
+                    if (coefficients[i].length === 0 || !this.isCoefficientValid(coefficients[i]))
+                    {
+                        isCoefficientsListComplete = false;
+                        break;
+                    }
+                }
+                if (isCoefficientsListComplete)
+                {
+                    alert("go api !");
+                }
+                else
+                {
+                    console.log("not yet ready");
+                }
+            },
             onBurgerMenuClick: function() {
                 
                 this.isMenuOpened = !this.isMenuOpened;
+            },
+            onStepButtonClick: function(step) {
+                
+                this.isMenuOpened = false;
+                this.currentState = "step-" + step;
             },
             typeWrite: function(el, message, delay) {
                     
@@ -46,6 +107,65 @@ define(function(require){
                     clearTimeout(timeoutID);
                     
                 }.bind(this), delay); 
+            }
+        },
+        watch: {
+            a0: function(value) {
+                
+                if (this.isCoefficientValid(value))
+                {
+                    this.isA0Invalid = false;
+                    this.observeCoefficients();
+                }
+                else
+                {
+                    this.isA0Invalid = true;
+                }
+            },
+            a1: function(value) {
+                
+                if (this.isCoefficientValid(value))
+                {
+                    this.isA1Invalid = false;
+                    this.observeCoefficients();
+                }
+                else
+                {
+                    this.isA1Invalid = true;
+                }
+            },
+            a2: function(value) {
+                
+                if (this.isCoefficientValid(value))
+                {
+                    this.isA2Invalid = false;
+                    this.observeCoefficients();
+                }
+                else
+                {
+                    this.isA2Invalid = true;
+                }
+            },
+            a3: function(value) {
+                
+                if (this.isCoefficientValid(value))
+                {
+                    this.isA3Invalid = false;
+                    this.observeCoefficients();
+                }
+                else
+                {
+                    this.isA3Invalid = true;
+                }
+            },
+            currentState: function(val) {
+                
+                if (val === "step-1")
+                {
+                    this.typeWrite(this.$$.step1Title, [
+                        "Saisissez les coefficients du polynôme pour obtenir les racines entières."
+                    ], 1000);
+                }
             }
         }
     });
